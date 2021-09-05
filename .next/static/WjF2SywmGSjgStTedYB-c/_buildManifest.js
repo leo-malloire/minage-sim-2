23a0015ec4dba1380aa507391cc454f7aa090336
leo-malloire <leo.malloire@gmail.com>
2021-09-05 21:12:19 +0200

    try to fix an error

---
+++ .next/static/WjF2SywmGSjgStTedYB-c/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/976-adcedcd36cdf6b1fcda8.js","static/css/da681e36977de1790493.css","static/chunks/pages/index-c86925fb23edea23604f.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
